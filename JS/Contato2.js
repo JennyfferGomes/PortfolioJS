34b4ef1181fd28106bf39f3be48c49b583d688b9
jennyffer <gajennyffer9@gmail.com>
2024-11-24 22:48:29 -0300

    Portfólio com JS

---
+++ JS/Contato2.js
@@ -0,0 +1,11 @@
+let pergunta1 = prompt(`Insira seu nome completo:`)
+let pergunta2 = prompt(`Insira seu número de telefone:`)
+let confirmacao = confirm('Você tem certeza?');
+
+if (confirmacao) {
+    alert(`Muito obrigada! Se puder, avalie o meu portifólio na página seguinte:`)
+} else {
+    alert('Você cancelou.');
+}
+
+
---
+++ JS/Main.js
@@ -0,0 +1,8 @@
+
+if (!sessionStorage.getItem('alertShown')) {
+   
+    alert('Olá! Este é o meu portfólio e você é muito bem vindo!');
+
+    sessionStorage.setItem('alertShown', 'true');
+  }
+  
